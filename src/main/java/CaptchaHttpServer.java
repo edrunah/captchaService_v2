7643facb4fc7ef06f6edb653a3898a97e976acbf
edrunah <32537550+edrunah@users.noreply.github.com>
2018-03-10 12:16:48 +0500

    промежуточный

--- src/main/java/CaptchaHttpServer.java
+++ src/main/java/CaptchaHttpServer.java
@@ -1,8 +1,15 @@
 import fi.iki.elonen.NanoHTTPD;
 import fi.iki.elonen.util.ServerRunner;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class CaptchaHttpServer extends NanoHTTPD {
@@ -15,7 +22,28 @@ public class CaptchaHttpServer extends NanoHTTPD {
 
     public static void main(String[] args) {
         CaptchaImageCreator.loadAWTLibraries();
-        ServerRunner.run(CaptchaHttpServer.class);
+
+        try {
+            CaptchaImageCreator.loadAWTLibraries();
+            Options options = new Options();
+            options.addOption("port", true, "port number to use");
+            options.addOption("web_threads", true, "maximum number of threads for the web server");
+
+            int port = 8080;
+            int webThreads = 10;
+
+            App app = new App(port);
+            app.setAsyncRunner(new BoundRunner(Executors.newFixedThreadPool(webThreads)));
+            app.start(100, false);
+            logger.info("waiting for connections on port " + port + " using a maximum of " +
+                webThreads + " web threads");
+
+        } catch (Throwable t) {
+            logger.log(Level.SEVERE, "Couldn't start server", t);
+            System.exit(1);
+        }
+
+//        ServerRunner.run(CaptchaHttpServer.class);
     }
 
     @Override
